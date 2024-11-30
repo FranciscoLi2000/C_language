9b77a9425694e85a3a4e8fd7a5bcd987a15926e4
FranciscoLi2000 <francisco2000li@icloud.com>
2024-11-30 17:49:49 +0000

    42

--- C00/ex04/ft_is_negative.c
+++ C00/ex04/ft_is_negative.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
-void    ft_is_negative(int n);
+void    ft_is_negative(int *n);
 
 void	ft_putchar(char c)
 {
 	write (1, &c, 1);
 }
 
-void    ft_is_negative(int n)
+void    ft_is_negative(int *n)
 {
 	if (n < 0)
 		ft_putchar('N');
@@ -17,8 +17,9 @@ void    ft_is_negative(int n)
 
 int	main(void)
 {
-    int n;
-    n = 124;
+    int n[1000];
+    write(1, "number is: ",11);
+	read(0, &n, 1);
 	ft_is_negative(n);
     ft_putchar('\n');
 	return (0);
